bc527d8ebd8625c3f52857ecf5524d1f01fbc867
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-09-10 09:52:58 +0530

    Changes to be committed: 	modified:   Lab2/shap1.cpp 	modified:   Lab2/shap1.exe

--- Lab2/shap1.cpp
+++ Lab2/shap1.cpp
@@ -1,17 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    int height = 9; // You can change the height of the triangle here
+    int height = 14; // You can change the height of the triangle here
+    string text;
+    
+    cout << "Enter the text to write inside the triangle: ";
+    getline(cin, text);
 
-    for(int i = 1; i <= height; ++i) {
-        for(int j = 1; j <= i; ++j) {
-            if (j == 1 || j == i || i == height) {
-                std::cout << "*";
+    int middleRow = height / 2 + 1;
+    int textStartIndex = (2 * middleRow - 1 - text.length()) / 2;
+
+    for (int i = 1; i <= height; ++i) {
+        // Print leading spaces
+        for (int j = i; j < height; ++j) {
+            cout << " ";
+        }
+        // Print stars and spaces inside the triangle
+        for (int j = 1; j <= (2 * i - 1); ++j) {
+            if (i == middleRow && j >= textStartIndex + 1 && j < textStartIndex + 1 + text.length()) {
+                cout << text[j - textStartIndex - 1];
+            } else if (j == 1 || j == (2 * i - 1) || i == height) {
+                cout << "*";
             } else {
-                std::cout << " ";
+                cout << " ";
             }
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 
     return 0;
