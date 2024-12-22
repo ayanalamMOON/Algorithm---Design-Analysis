3eb1e6d3ac8cd013c34f42b76c7ee496f70dabd7
Ayan Alam <83778128+ayanalamMOON@users.noreply.github.com>
2024-12-22 22:32:43 +0530

    modified:   project/algorithm_performance.log 	new file:   project/error.log 	modified:   project/final.cpp 	modified:   project/test.exe

--- project/final.cpp
+++ project/final.cpp
@@ -113,3 +113,36 @@ function search2DArray(arr, rows, cols, target)
  * - Dynamic programming
  * - String matching algorithms
  */
+
+
+//write a program to print fibbonaci series
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << ", ";
+    }
+    return 0;
+}
